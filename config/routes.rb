72dd6edaf7e5b84a77b2e1d39033ec4f87518dfd
Huzaifa Raghav <windowpane1712@gmail.com>
2021-12-28 19:20:28 +0800

    Nest tasks routes under categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :categories
+  resources :categories do
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
