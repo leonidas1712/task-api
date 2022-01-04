7ad2bcb7a7dfc68fd02a6776fbd1db710cf11577
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-04 22:53:57 +0800

    Permit due_date and priority params for POST categories/:id/tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -62,6 +62,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :due_date, :priority)
     end
 end
