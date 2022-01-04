e2b15c9b9099eb768ea16d2f1d770e4cb56cbf48
Huzaifa Raghav <windowpane1712@gmail.com>
2022-01-04 18:10:42 +0800

    Update seeds.rb with datetime, descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Category.destroy_all
+d = DateTime
 
 cat1 = Category.new 
 cat1.name = "First"
@@ -18,16 +19,19 @@ cat2.save
 t1 = cat1.tasks.build 
 t1.name = "Task1, Cat1"
 t1.description = "Task1 description "
+t1.due_date = DateTime.now
 t1.save 
 
 t2 = cat1.tasks.build
 t2.name = "Task2, Cat1"
 t2.description = "Buy laundry"
+t2.due_date = DateTime.now + 9.days
 t2.save 
 
 t2_1 = cat2.tasks.build 
 t2_1.name = "Task 1, Cat 2"
 t2_1.description = "Once finished, update teammates and finalize plan"
+t2_1.due_date = DateTime.now + 1.weeks
 t2_1.save 
 
 t2_2 = cat2.tasks.build 
